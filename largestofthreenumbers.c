af366dc3f32198619e5ac2e25c9aa96ec2e41dad
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-10-21 11:59:33 +0530

    Create largestofthreenumbers.c

---
+++ largestofthreenumbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the values of a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+    {
+        printf("%d is largest",a);
+        
+    }
+    }
+    else if(b>c)
+    {
+        printf("%d is largest",b);
+    }
+    else
+    {
+        printf("%d is largest",c);
+    }
+    return 0;
+}
