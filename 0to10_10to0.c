0c9912ee6f7e986ce00978fd53adbe7fa1afccbb
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-12-23 14:29:26 +0530

    Create 0to10_10to0.c

---
+++ 0to10_10to0.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("the numbers from 0 to 10 is :\n");
+    for(i=0;i<=10;++i)
+    {
+    printf("%d\n",i);
+    }
+    printf("the reverse order of numbers from 10 to 0 \n");
+    for(i=10;i>=0;--i)
+    {
+    printf("%d\n",i);
+   }
+}
