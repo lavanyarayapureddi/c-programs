dd4024f488a3fc49c817b0cdc3db714fb8e539f6
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-12-25 15:16:15 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
+#include<math.h>
 void main()
 {
-    int n,temp,remainder,sum=0;
+    int n,temp,remainder,sum=0,x;
     printf("enter the value of n:");
     scanf("%d",&n);
     temp=n;
+    x=floor(log10(n)+1);
     while(n!=0)
     {
        remainder=n%10;
-       sum=sum+(remainder*remainder*remainder);
+       sum=sum+pow(remainder,x);
        n=n/10;
     }
     if(sum==temp)
