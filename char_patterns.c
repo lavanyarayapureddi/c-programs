01e74b9cec69aea8870e22a35f500b5ee3e91d92
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-12-30 11:27:02 +0530

    Create char_patterns.c

---
+++ char_patterns.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    char n='a';
+    {
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("%c\t",n);
+        }
+        n++;
+        printf("\n");
+    }
+    }
+}
