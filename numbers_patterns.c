6c8342bc5454a521042cba9a3ce52e50a7cae52b
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-12-30 11:28:50 +0530

    Create numbers_patterns.c

---
+++ numbers_patterns.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    {
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("%d\t",n);
+        }
+        n++;
+        printf("\n");
+    }
+    }
+}
