8bedd133df7cba74038c6948505305658729be73
lavanyarayapureddi <lavanyarlvns@gmail.com>
2024-12-30 12:09:56 +0530

    Create reverse_of_numbers_patterns.c

---
+++ reverse_of_numbers_patterns.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n=5;
+    {
+    for(i=1;i<=5;++i)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("%d\t",n);
+        }
+        n--;
+        printf("\n");
+    }
+    }
+}
