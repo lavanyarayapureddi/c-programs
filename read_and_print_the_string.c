0a3a89f5d0702a7d395d2626019f9a30b8020b16
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-01-20 19:30:39 +0530

    Create read_and_print_the_string.c

---
+++ read_and_print_the_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[100];
+    int size;
+    printf("enter the size of a string:");
+    scanf("%d",&size);
+    printf("enter the str:");
+    scanf("%s",str);
+    printf("%s",str);
+}
